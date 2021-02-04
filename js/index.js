784cce90de88ad9d697e9cbec0801458ce139938
zhangyihua <zhangyihua_2020@qq.com>
2021-02-04 17:13:50 +0800

    电单车充电完成

--- js/index.js
+++ js/index.js
@@ -37,10 +37,10 @@ if (ratioPage < 0.6) {
 window.onload = function () {
     var swiper = new Swiper('.swiper-container', {
         centeredSlides: true,
-        autoplay: {
-            delay: 10000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 10000,
+        //     disableOnInteraction: false,
+        // },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
